f040a9055ae6fcbf8850a573807f7a53d5f53e56
camow7 <cameron.owen@live.com.au>
2019-11-12 22:40:45 +1100

    Started Angularfire db integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,33 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 const paths: {}[] = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent}
 ]
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBFUSBY61oC3b1E45eGxD-arFQNPLdyGpg",
+  authDomain: "blastfurnacedemo.firebaseapp.com",
+  databaseURL: "https://blastfurnacedemo.firebaseio.com",
+  projectId: "blastfurnacedemo",
+  storageBucket: "blastfurnacedemo.appspot.com",
+  messagingSenderId: "518923278740",
+  appId: "1:518923278740:web:418a76291ff5a1db2dd781",
+  measurementId: "G-Q0BH7H85ED"
+};
+
 @NgModule({
   imports:      [ BrowserModule, 
                   FormsModule,
                   MDBBootstrapModule.forRoot(),
-                  RouterModule.forRoot(paths) 
+                  RouterModule.forRoot(paths),
+                  AngularFireModule.initializeApp(firebaseConfig),
+                  AngularFireDatabaseModule,
                 ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { email: string, password: string }
 
 @Component({
   selector: 'app-login',
@@ -8,13 +12,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   email: string;
   password: string;
-  constructor() { }
+  private itemDoc: AngularFirestoreDocument<Item>;
+  item: Observable<Item>;
+  constructor(private afs: AngularFirestore) {
+    this.itemDoc = afs.collection("users").doc("testUser");
+    this.item = this.itemDoc.valueChanges();
+  }
+  update(item: Item) {
+    this.itemDoc.update(item);
+  }
 
   ngOnInit() {
   }
 
   onSubmit () {
     console.log(this.email + ", " + this.password);
+    this.itemDoc.update({email: this.email, password: this.password});
   }
 
 }
