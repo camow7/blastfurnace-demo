3d180cd2feb41fa6f73ed6033b3a2fa9885ebbfe
camow7 <cameron.owen@live.com.au>
2019-11-12 22:13:57 +1100

    Added basics to the site before firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const paths: {}[] = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent}
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, 
+                  FormsModule,
+                  MDBBootstrapModule.forRoot(),
+                  RouterModule.forRoot(paths) 
+                ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
